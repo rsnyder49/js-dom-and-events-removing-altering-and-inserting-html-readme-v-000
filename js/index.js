02875c8534c69d007f8aa9c98b49558146b6c57f
Robert Snyder <r.snyder49@gmail.com>
2018-12-27 02:03:36 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -6,3 +6,9 @@ ele.style.backgroundColor = '#f9f9f9';
 ele.style.textAlign = 'center';
 
 document.body.appendChild(ele);
+
+var ul = document.createElement('ul');
+
+for (let i = 0; i < 3; i++) {
+  let li = dou
+}
