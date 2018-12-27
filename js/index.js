c2b4606c16440f7d4edf7456781c912a22862204
Robert Snyder <r.snyder49@gmail.com>
2018-12-27 02:01:56 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -2,3 +2,6 @@
 //you can use this to paste in js code and have it run when index.html is loaded
 var ele = document.createElement('div');
 ele.innerHTML = "Hello, DOM!";
+ele.style.backgroundColor = '#f9f9f9';
+
+document.body.appendChild(ele);
