225c5481b65dd0a2491a161036e7cf103ea6f0c5
Robert Snyder <r.snyder49@gmail.com>
2018-12-27 02:04:23 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -10,5 +10,10 @@ document.body.appendChild(ele);
 var ul = document.createElement('ul');
 
 for (let i = 0; i < 3; i++) {
-  let li = dou
+  let li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+ 
+ele.appendChild(ul);
 }
