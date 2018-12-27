33c4cdadd977c40a50e166d6f5d3b0922a50a2e6
Robert Snyder <r.snyder49@gmail.com>
2018-12-27 02:05:46 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,24 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+var ele = document.createElement('div');
+ele.innerHTML = "Hello, DOM!";
+ele.style.backgroundColor = '#f9f9f9';
+ele.style.textAlign = 'center';
+
+document.body.appendChild(ele);
+
+var ul = document.createElement('ul');
+
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+ 
+ele.appendChild(ul);
+
+ul.style.textAlign = 'left';
+
+ul.removeChild(ul.querySelector('li:nth-child(2)'));
+
+ul.remove();
